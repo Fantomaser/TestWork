409c967248f5fb89c298a69b0814319e6837fa17
Fantomaser <49395532+Fantomaser@users.noreply.github.com>
2020-01-18 16:34:53 +0300

    sec

--- WebList/app.js
+++ WebList/app.js
@@ -2,6 +2,22 @@ var app = new Vue({
     el: '#app',
     data: {
       message: 'Привет, Vue!',
-      InpFormMessage: 'Введите сообщение'
+      InpFormMessage: 'Введите сообщение',
+      btnName: 'Сгенерировать',
+      OutpKeyMess: 'Ключ',
+      OutpURLMess: 'Адрес',
+      Message: ""
+    },
+    methods:{
+      CheckText: function(){
+        console.log('Chekau', this.Message)
+              // GET /someUrl
+        this.$http.post('http://localhost:9090//makeText', {MSG : this.Message}).then(res => {
+            console.log('Generation message', res)
+
+        }, res => {
+            console.log("SendError", res)
+        });
+      }
     }
   })
